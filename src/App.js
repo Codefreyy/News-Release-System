966fcfb147094a846e232d0a1d746d884d44488b
Codefreyy <yukiopang@163.com>
2022-08-03 15:26:01 +0800

    feat: 侧边栏状态持久化 redux-persist

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css'
 import IndexRouter from './router/IndexRouter'
 import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from './redux/store'
 
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
-        <IndexRouter />
-      </HashRouter>
+      <PersistGate loading={null} persistor={persistor}>
+
+        <HashRouter>
+          <IndexRouter />
+        </HashRouter>
+      </PersistGate>
     </Provider>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,33 @@
 import { createStore, combineReducers } from 'redux'
 import { CollapsedReducer } from './reducers/CollapsedReducer'
 import { LoadingReducer } from './reducers/LoadingReducer'
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+const persistConfig = {
+    key: 'kerwin',
+    storage,
+    blacklist: ['LoadingReducer']
+}
 const reducer = combineReducers({
     CollapsedReducer,
     LoadingReducer
 })
+const persistedReducer = persistReducer(persistConfig, reducer)
 
-const store = createStore(reducer)
 
+const store = createStore(persistedReducer);
+const persistor = persistStore(store)
+export {
+    store,
+    persistor
+}
 
-export default store
+/*
+ store.dispatch()
+
+ store.subsribe()
+
+*/
